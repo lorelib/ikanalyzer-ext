fd6bda43ec3a33f304f93f48eb95cb42b0a9f3f2
listening <893979653@qq.com>
2017-03-10 14:29:13 +0800

    update package and use custom dic to test

---
+++ src/test/java/com/lorelib/analyzer/sample/IKAnalyzerTest.java
@@ -0,0 +1,56 @@
+package com.lorelib.analyzer.sample;
+
+import com.lorelib.analyzer.core.IKSegmenter;
+import com.lorelib.analyzer.core.Lexeme;
+import com.lorelib.analyzer.lucene.IKAnalyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.*;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.StringReader;
+
+/**
+ * Created by listening on 2016/12/28.
+ */
+public class IKAnalyzerTest {
+    @Test
+    public void testWithLucene() throws IOException {
+        String word = "重庆江北区重庆市江北区海尔路北门新村181号兰溪小区30幢3单元6-1";
+        IKAnalyzer analyzer = new IKAnalyzer(true);
+        TokenStream ts = analyzer.tokenStream("content", new StringReader(word));
+        //获取词元位置属性
+        OffsetAttribute offset = ts.addAttribute(OffsetAttribute.class);
+        //获取词元文本属性
+        CharTermAttribute term = ts.addAttribute(CharTermAttribute.class);
+        //获取词元文本属性
+        TypeAttribute type = ts.addAttribute(TypeAttribute.class);
+        KeywordAttribute keyword = ts.addAttribute(KeywordAttribute.class);
+        FlagsAttribute flags = ts.addAttribute(FlagsAttribute.class);
+
+        //重置TokenStream（重置StringReader）
+        ts.reset();
+        //迭代获取分词结果
+        while (ts.incrementToken()) {
+            System.out.print(term.toString() + "  ");
+            /*System.out.println(term.toString() + "  " + offset.toString() + "  " + type.toString()
+                    //+ "  " + keyword.toString() + "  " + flags.toString()
+            );*/
+        }
+        ts.end();
+        ts.close();
+    }
+
+    @Test
+    public void testWithNoLucene() throws IOException {
+        // 检索内容
+        String word = "北京市海淀区海淀人大西门万泉庄苏州街77号院苏州街77号";
+        StringReader reader = new StringReader(word);
+        IKSegmenter ik = new IKSegmenter(reader, true);
+        Lexeme lexeme = null;
+        while ((lexeme = ik.next()) != null) {
+            System.out.println(lexeme.getLexemeText());
+        }
+        reader.close();
+    }
+}
